4c3e277c6a3e5f0cf0c57a90b05b868451c14ee6
subo-9439 <kws3363@gmail.com>
2022-02-07 14:56:45 +0900

    vue3 예제 시작

---
+++ src/routes/index.js
@@ -0,0 +1,26 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from './Home'
+import About from './About'
+import Movie from './Movie'
+
+export default createRouter({
+  // Hash, History
+  // https://google.com/#/search <- Hash mode
+  history: createWebHashHistory(),
+  // pages
+  // https://google.com/
+  routes: [
+    {
+      path: '/',
+      component: Home
+    },
+    {
+      path: '/movie',
+      component: Movie
+    },
+    {
+      path: '/about',
+      component: About
+    }
+  ]
+})
