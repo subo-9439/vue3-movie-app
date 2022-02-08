2a849b815a9a66877386cdd9388cba01d0bf257e
subo-9439 <kws3363@gmail.com>
2022-02-08 23:31:36 +0900

    Search component 만들기

---
+++ src/components/Search.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="container">
+    <input 
+      v-model="title" 
+      class="form-control"
+      type="text" 
+      placeholder="Search for Movies, Series & more"
+      @keyup.enter="apply" />
+    <div class="selects">
+      <select
+        v-for="filter in filters"
+        v-model="$data[filter.name]"
+        :key="filter.name"
+        class="form-select">
+        <option 
+          v-if="filter.name === 'year'"
+          value="">
+          All Years
+        </option>
+        <option
+          v-for="item in filter.items"
+          :key="item">
+          {{ item }}
+        </option>
+      </select>
+    </div>
+    <button 
+      class="btn btn-primary" 
+      @click="apply">
+      Apply 
+      </button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      title: '',
+      type: 'movie',
+      number: 10,
+      year: '',
+      filters: [
+        {
+          name: 'type',
+          items: ['movie', 'seires', 'episode']
+        },
+        {
+          name: 'number',
+          items: [10, 20, 30]
+        },
+        {
+          name: 'year',
+          //즉시 실행 함수
+          items: (() => {
+            const years = []
+            const thisYear = new Date().getFullYear() // 2022
+            for (let i = thisYear; i >= 1985; i-=1){
+              years.push(i)
+            }
+            return years
+          })()
+        }
+      ]
+    }
+  },
+  methods: {
+    async apply() {
+      // Search movies...
+      const OMDB_API_KEY = '2574b6bd' 
+      const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${this.title}&type=${this.type}&y=${this.year}&page=1`)
+      console.log(res)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  > * {
+    margin-right: 10px;
+    font-size: 15px;
+    &:last-child {
+      margin-right: 0px;
+    }
+  }
+  .selects {
+    display: flex;
+    select {
+      width: 120px; 
+      margin-right: 10px; 
+      &:last-child {
+        margin-right: 0;
+      }
+    }
+  }
+  .btn {
+    width: 120px;
+    height: 50px;
+    font-weight: 700;
+    flex-shrink: 0;
+  }
+}
+</style>
